f44af2d3287f72a08b8e8f60cebbb093e1ca218d
julius <nyerereoffice@gmail.com>
2024-08-16 12:38:01 +0300

    Login Page UI

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,58 @@
-import './Login.css';
+import { useState } from "react";
+import assets from "../../assets/assets";
+import "./Login.css";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [currState, setCurrState] = useState("Sign up");
 
-export default Login
+  return (
+    <div className="login">
+      <img src={assets.logo_big} alt="" />
+      <form action="" className="login-form">
+        <h2>{currState}</h2>
+        {currState === "Sign up" ? (
+          <input
+            type="text"
+            placeholder="username"
+            className="form-input"
+            required
+          />
+        ) : null}
+        <input
+          type="email"
+          placeholder="Email address"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="form-input"
+          required
+        />
+        <button type="submit">
+          {currState === "Sign up" ? "Create account" : "Login now"}
+        </button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+        <div className="login-forgot">
+          {currState === "Sign up" ? (
+            <p className="login-toggle">
+              Already have an account{" "}
+              <span onClick={() => setCurrState("Log in")}>Login here</span>
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Create an account{" "}
+              <span onClick={() => setCurrState("Sign up")}>click here</span>
+            </p>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
