630b23add85d387107c69b5bf542f3e8e35dd4e7
julius <nyerereoffice@gmail.com>
2024-08-17 04:46:39 +0300

    LeftSidebar UI

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,35 @@
+import './LeftSidebar.css';
+import assets from '../../assets/assets'
+
+
+const LeftSidebar = () => {
+  return (
+    <div className='ls'>
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className='logo' alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} className='' alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} className='' alt="" />
+          <input type="text" placeholder='Search here..' />
+        </div>
+      </div>
+      <div className="ls-list">
+        {Array(12).fill("").map((item, index)=>(
+          <div key={index} className="friends">
+          <img src={assets.profile_img} className='' alt="" />
+          <div className="">
+            <p>Richard Sanford</p>
+            <span>Hello, How are you?</span>
+          </div>
+        </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default LeftSidebar
