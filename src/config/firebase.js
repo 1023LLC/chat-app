071ff341690cb2d22184a6798777ccede049e232
julius <nyerereoffice@gmail.com>
2024-08-17 12:54:19 +0300

    Navigation

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 
@@ -51,4 +51,15 @@ const login = async (email, password) =>{
     }
 }
 
-export {signup, login}
+
+const logout = async () =>{
+    try {
+        await signOut(auth)
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "))
+    }
+}
+
+
+export {signup, login, logout, auth, db}
