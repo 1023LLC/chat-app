385a0fcfb051644273fb4c7e69acb5ccb4be9770
julius <nyerereoffice@gmail.com>
2024-08-20 13:24:04 +0300

    Search Functionality

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,44 +1,81 @@
-import './LeftSidebar.css';
-import assets from '../../assets/assets'
-import { useNavigate } from 'react-router-dom';
-
+import "./LeftSidebar.css";
+import assets from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { AppContext } from "../../context/AppContext";
+import { useContext, useState } from "react";
 
 const LeftSidebar = () => {
+  const navigate = useNavigate();
+  const { userData } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+  const [showSearch, setShowSearch] = useState(false);
 
-  const navigate = useNavigate()
+  const inputHandler = async (e) => {
+    try {
+      const input = e.target.value;
+      if (input) {
+        setShowSearch(true);
+        const userRef = collection(db, "users");
+        const q = query(userRef, where("username", "===", input.toLowerCase()));
+        const querySnap = await getDocs(q);
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          setUser(querySnap.docs[0].data());
+        } else {
+          setUser(null);
+        }
+      } else {
+        setShowSearch(false);
+      }
+    } catch (error) {}
+  };
 
   return (
-    <div className='ls'>
+    <div className="ls">
       <div className="ls-top">
         <div className="ls-nav">
-          <img src={assets.logo} className='logo' alt="" />
+          <img src={assets.logo} className="logo" alt="" />
           <div className="menu">
-            <img src={assets.menu_icon} className='' alt="" />
+            <img src={assets.menu_icon} className="" alt="" />
             <div className="sub-menu">
-              <p onClick={()=>navigate('/profile')}>Edit Profile</p>
+              <p onClick={() => navigate("/profile")}>Edit Profile</p>
               <hr />
               <p>Logout</p>
             </div>
           </div>
         </div>
         <div className="ls-search">
-          <img src={assets.search_icon} className='' alt="" />
-          <input type="text" placeholder='Search here..' />
+          <img src={assets.search_icon} className="" alt="" />
+          <input
+            onChange={inputHandler}
+            type="text"
+            placeholder="Search here.."
+          />
         </div>
       </div>
       <div className="ls-list">
-        {Array(12).fill("").map((item, index)=>(
-          <div key={index} className="friends">
-          <img src={assets.profile_img} className='' alt="" />
-          <div className="">
-            <p>Richard Sanford</p>
-            <span>Hello, How are you?</span>
+        {showSearch && user ? (
+          <div className="friends add-user">
+            <img src={user.avatar} alt="user-avatar" />
+            <p>{user.name}</p>
           </div>
-        </div>
-        ))}
+        ) : (
+          Array(12)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="friends">
+                <img src={assets.profile_img} className="" alt="" />
+                <div className="">
+                  <p>Richard Sanford</p>
+                  <span>Hello, How are you?</span>
+                </div>
+              </div>
+            ))
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
